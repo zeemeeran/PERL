426da5c85b456aee2628d0a2e9a121b1bd29d2d8
Zahida Meeran <dev.zmeeran@gmail.com>
2022-08-09 14:46:36 -0400

    Perl first program

---
+++ HelloWorld.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use warnings;
+
+
+print("Hello World!\n");
+
+#$dirname = $ARGV[0];
+
+$dirname = <STDIN>;
+
+print("We will scan the credit card numbers from this directory: $dirname\n");
